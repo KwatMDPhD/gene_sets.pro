7a204b4d7b08051c5a4a60e8d978f37dbd2f3c42
KwatMDPhD <kwat.me@icloud.com>
2022-02-19 13:41:52 -0700

    Housekeeping

--- code/_.jl
+++ code/_.jl
@@ -1,13 +1,15 @@
-# ---
-# Default lean project things
+# ----------------------------------------------------------------------------------------------- #
+# Default lean-project things
 
-#using LeanProject
+using LeanProject
 
 se = joinpath(dirname(@__DIR__), "input", "setting.json")
 
-#PAR, PAI, PAC, PAO = LeanProject.get_project_path(se)
+PAR, PAI, PAC, PAO = LeanProject.get_project_path(se)
 
-#SE = LeanProject.read_setting(se)
+SE = LeanProject.read_setting(se)
 
-# ---
-# Project specific things
+# ----------------------------------------------------------------------------------------------- #
+# Project-specific things
+
+using OnePiece
